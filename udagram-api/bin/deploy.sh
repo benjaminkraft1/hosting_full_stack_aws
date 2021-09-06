38bec911840ea9506212e5964032174317d5e2ca
Kraft Benjamin (059) <>
2021-09-06 15:54:03 +0200

    fix deployment to beanstalk

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-aws s3 cp ./www/Archive.zip http://udagramenv-env.eba-wcahkxjc.us-east-1.elasticbeanstalk.com/
+aws s3 cp ./www/Archive.zip s3://elasticbeanstalk-us-east-1-532545066122/
 aws elasticbeanstalk create-application-version --application-name udagram-env --source-bundle S3Bucket="elasticbeanstalk-us-east-1-532545066122"
 aws elasticbeanstalk update-environment --application-name udagram-env --environment-name Udagramenv-env
