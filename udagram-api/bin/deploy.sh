ac17db49c8e38fc43602e2d1a13dfa86bfe5edde
Kraft Benjamin (059) <>
2021-09-06 15:00:04 +0200

    fix deploy

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
 aws s3 cp ./www/Archive.zip $S3_BUCKET_SERVER
-aws elasticbeanstalk create-application-version --application-name $EB_APP --version-label <<pipeline.git.revision>> --source-bundle S3Bucket="$EB_BUCKET",S3Key="Archive.zip"
-aws elasticbeanstalk update-environment --application-name $EB_APP --environment-name $EB_ENV --version-label <<pipeline.git.revision>>
+aws elasticbeanstalk create-application-version --application-name udagram-env --version-label <<pipeline.git.revision>> --source-bundle S3Bucket="elasticbeanstalk-us-east-1-532545066122",S3Key="Archive.zip"
+aws elasticbeanstalk update-environment --application-name udagram-env --environment-name Udagramenv-env --version-label <<pipeline.git.revision>>
